b6b1b44447e0df9b8a9211b89c102c3ead18cf2d
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-08 15:54:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * description
+ * Description: Write a C program that prints exactly"Programming is like building a multilingual puzzle,followed by a new line"
  *
- * return (0)
- */*
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {

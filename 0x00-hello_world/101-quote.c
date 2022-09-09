e2ebf8152e8be36931793eed27e87b441a0f817a
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-08 20:20:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * include <unistd.h>
- *Return: Always 1
+ * main - Entry point
+ * Return: Always 1
  *
  */
 
-int main(void)
+int main (void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);

b2e18c75858563e4be6ae536abe70e1b2bb27d3d
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-13 10:49:25 -0700

    0_putchar.c

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - A programme that prints _putchar
+ * Return: Always 0 (Success)
+ * Description - The program should return 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}

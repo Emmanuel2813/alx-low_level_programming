d10e458c7a3f709ddd71c852f9785ed8034181dd
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-15 12:45:39 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -16,8 +16,8 @@ void print_most_numbers(void)
 	for (i = 0; i < 10; i++)
 
 	{
-	if (i != 2  && i != 4)
-	_putchar(i);
+		if (i != 2  && i != 4)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@ isupper
 isdigit
 Collaboration is multiplication
 The numbers speak for themselves
+I believe in numbers and signs

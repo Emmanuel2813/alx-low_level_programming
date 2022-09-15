fe7ce6bd36197528fc7a0f526fef08936c25f261
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-15 05:19:57 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Multiplies two integers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i <= 0; i >= 0;)
+	{
+		for (j <= 0; j >= 0;)
+			_putchar ("%d\n", i * j);
+	}
+
+	return (0);
+}

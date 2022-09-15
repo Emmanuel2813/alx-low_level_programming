a56f0490b1425349bd44601b9b863acb6e8059da
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-15 10:19:54 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,16 @@
 
 /**
  * void print_numbers - A function that prints numbers (0 to 9).
- * Return: Always 0 (Successful)
+ * follow by a new line
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
-
 	_putchar('\n');
 }

6f1c1ae3a8060ae85c9b31c017aed9da26ee35d5
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-15 17:14:44 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,14 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
+
 void print_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-_putchar(i + '0');
-}
-
-_putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }

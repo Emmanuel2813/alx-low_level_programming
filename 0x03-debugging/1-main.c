238a1fdedd13661075dddc417593e14676f1ba00
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-15 19:47:54 -0700

    3-print_remaining_days.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinitr loop incoming :(\n");
+
+	i = 0;
+	/*while (i < 1o)*/
+	/*{*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
---
+++ 0x03-debugging/positve_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is postive\n", i);
+}

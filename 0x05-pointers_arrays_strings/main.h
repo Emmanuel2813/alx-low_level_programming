71c80b0cc7ac2dabc10f11436a19aded84d6ce21
Aroyewun Emmanuel <aroyewunemmanuel@gmail.com>
2022-09-19 10:11:57 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,15 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+
+#include <stdio.h>
+
+
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +20,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
